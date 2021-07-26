8f5e91037544a21e1a83be1f48dace5c71dfe25e
Victor Bolaños Solís <viktorb.132@gmail.com>
2021-07-26 11:58:21 -0500

    Redux configuration

--- src/CalendarApp.js
+++ src/CalendarApp.js
@@ -1,81 +1,15 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Approuter } from './route/Approuter';
-import { Calendar, momentLocalizer } from 'react-big-calendar';
-import moment from 'moment';
 import 'moment/locale/es-mx'
 import 'react-big-calendar/lib/css/react-big-calendar.css';
-import { messages } from './helpers/claedar-messages-es';
-import { CalendarEvent } from './components/calendar/CalendarEvent';
-import { CalendarModal } from './components/calendar/CalendarModal';
-
-moment.locale('es')
-const localizer = momentLocalizer(moment) // or globalizeLocalizer
-
-const events = [{
-    title: 'Spacieal day',
-    start: moment().toDate(),
-    end: moment().add(2, 'hours').toDate(),
-    bgcolor: '#fafafa',
-    notes: 'buy',
-    user: {
-        _id: '1234',
-        name: 'Victor'
-    }
-}]
+import { Provider } from 'react-redux';
+import { store } from './store/store';
 
 export const CalendarApp = () => {
 
-    const [lastView, setLastView] = useState(localStorage.getItem('lastView') || 'month')
-
-    const onDoubleClick = (e) => {
-        console.log(e);
-    }
-
-    const onSelectEvent = (e) => {
-        console.log(e);
-    }
-
-    const onViewChange = (e) => {
-        // console.log(e);
-        setLastView(e);
-        localStorage.setItem('lastView', e);
-    }
-
-    const eventStyleGetter = (event, start, end, isSelected) => {
-        // console.log(event, start, end, isSelected);
-        const style = {
-            backgroundColor: '#367CF7',
-            borderRadious: '0px',
-            opacity: 0.8,
-            display: 'block',
-            color: 'white'
-        }
-        return {
-            style
-        }
-    }
-
     return (
-        <div className="calendar-screen">
+        <Provider store={ store }>
             <Approuter />
-
-            <Calendar
-                localizer={localizer}
-                events={events}
-                startAccessor="start"
-                endAccessor="end"
-                messages={ messages }
-                eventPropGetter={ eventStyleGetter }
-                onDoubleClickEvent={ onDoubleClick }
-                onSelectEvent={ onSelectEvent }
-                onView={ onViewChange }
-                view={ lastView }
-                components={{
-                    event: CalendarEvent
-                }}
-            />
-
-            <CalendarModal />
-        </div>
+        </Provider>
     )
 }
--- src/components/calendar/CalendarScreen.js
+++ src/components/calendar/CalendarScreen.js
@@ -1,10 +1,79 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Navbar } from '../ui/Navbar'
+import { CalendarEvent } from './CalendarEvent';
+import { CalendarModal } from './CalendarModal';
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import moment from 'moment';
+import { messages } from '../../helpers/claedar-messages-es';
+
+moment.locale('es')
+const localizer = momentLocalizer(moment) // or globalizeLocalizer
+
+const events = [{
+    title: 'Spacieal day',
+    start: moment().toDate(),
+    end: moment().add(2, 'hours').toDate(),
+    bgcolor: '#fafafa',
+    notes: 'buy',
+    user: {
+        _id: '1234',
+        name: 'Victor'
+    }
+}]
 
 export const CalendarScreen = () => {
+
+    const [lastView, setLastView] = useState(localStorage.getItem('lastView') || 'month')
+
+    const onDoubleClick = (e) => {
+        console.log(e);
+    }
+
+    const onSelectEvent = (e) => {
+        console.log(e);
+    }
+
+    const onViewChange = (e) => {
+        // console.log(e);
+        setLastView(e);
+        localStorage.setItem('lastView', e);
+    }
+
+    const eventStyleGetter = (event, start, end, isSelected) => {
+        // console.log(event, start, end, isSelected);
+        const style = {
+            backgroundColor: '#367CF7',
+            borderRadious: '0px',
+            opacity: 0.8,
+            display: 'block',
+            color: 'white'
+        }
+        return {
+            style
+        }
+    }
+
     return (
-        <div>
+        <div className="calendar-screen">
             <Navbar />
+
+            <Calendar
+                localizer={localizer}
+                events={events}
+                startAccessor="start"
+                endAccessor="end"
+                messages={ messages }
+                eventPropGetter={ eventStyleGetter }
+                onDoubleClickEvent={ onDoubleClick }
+                onSelectEvent={ onSelectEvent }
+                onView={ onViewChange }
+                view={ lastView }
+                components={{
+                    event: CalendarEvent
+                }}
+            />
+
+            <CalendarModal />
         </div>
     )
 }
